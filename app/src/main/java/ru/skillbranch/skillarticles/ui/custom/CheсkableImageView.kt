972656805e7f8c781652f53d1fadf9cf87cc3a0e
Darkhan <drn.a4j@gmail.com>
2020-07-26 15:22:47 +0600

    fix ChekableImageView to CheсkableImageView

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/CheсkableImageView.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/CheсkableImageView.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.Checkable
 import android.widget.ImageView
 
-class ChekableImageView @JvmOverloads constructor(
+class CheсkableImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
